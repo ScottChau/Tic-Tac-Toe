88a3747f3feb2e3d1f332fcf27c9ba23ef57149c
Scott Chau <scottshing@yahoo.com.hk>
2022-09-07 17:22:41 -0400

    basic function set up

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const allBlank = document.querySelectorAll(".blank");
 
-allBlank.forEach((blank) =>
+const gameBoard = {
+  gameBoard: [...allBlank],
+  playerX: true,
+  playerO: false,
+};
+
+allBlank.forEach((blank, i) =>
   blank.addEventListener("click", function (e) {
     e.preventDefault();
-    blank.textContent = "X";
+    if (gameBoard.playerX === true) {
+      blank.textContent = "X";
+      gameBoard.playerX = false;
+      gameBoard.playerO = true;
+    } else {
+      blank.textContent = "O";
+      gameBoard.playerX = true;
+      gameBoard.playerO = false;
+    }
+    console.log(gameBoard.gameBoard[i].textContent);
   })
 );
