7a4f6ef3d64c58e9a965e66830cf527934a36243
Scott Chau <scottshing@yahoo.com.hk>
2022-09-08 20:10:15 -0400

    basic  win determination function completed

--- script.js
+++ script.js
@@ -1,23 +1,87 @@
+const restartBtn = document.querySelector(".restart");
 const allBlank = document.querySelectorAll(".blank");
+const q = document.querySelector(".q");
+const w = document.querySelector(".w");
+const e = document.querySelector(".e");
+const a = document.querySelector(".a");
+const s = document.querySelector(".s");
+const d = document.querySelector(".d");
+const z = document.querySelector(".z");
+const x = document.querySelector(".x");
+const c = document.querySelector(".c");
 
 const gameBoard = {
-  gameBoard: [...allBlank],
-  playerX: true,
-  playerO: false,
+  position: [...allBlank],
+  playerX: "X",
+  playerO: "O",
 };
+
+let turn = false;
+
+function checkWin(player) {
+  if (turn === null) {
+    return alert("Game is over, hit restart to start a new game");
+  } else if (
+    (q.textContent === player &&
+      w.textContent === player &&
+      e.textContent === player) ||
+    (a.textContent === player &&
+      s.textContent === player &&
+      d.textContent === player) ||
+    (z.textContent === player &&
+      x.textContent === player &&
+      c.textContent === player) ||
+    (q.textContent === player &&
+      a.textContent === player &&
+      z.textContent === player) ||
+    (w.textContent === player &&
+      s.textContent === player &&
+      d.textContent === player) ||
+    (e.textContent === player &&
+      d.textContent === player &&
+      c.textContent === player) ||
+    (q.textContent === player &&
+      s.textContent === player &&
+      c.textContent === player) ||
+    (e.textContent === player &&
+      s.textContent === player &&
+      z.textContent === player)
+  ) {
+    alert(`Player${player} Win! `);
+    turn = null;
+  }
+}
+
+function restart() {
+  allBlank.forEach((blank) => (blank.textContent = ""));
+  turn = false;
+}
+
+function displayControl(blank, i) {
+  const content = gameBoard.position[i].textContent;
+  const playerX = gameBoard.playerX;
+  const playerO = gameBoard.playerO;
+
+  if (content === "X" || content === "O") {
+    return;
+  } else if (turn === false) {
+    blank.textContent = playerX;
+    turn = true;
+  } else if (turn === true) {
+    blank.textContent = playerO;
+    turn = false;
+  }
+}
 
 allBlank.forEach((blank, i) =>
   blank.addEventListener("click", function (e) {
     e.preventDefault();
-    if (gameBoard.playerX === true) {
-      blank.textContent = "X";
-      gameBoard.playerX = false;
-      gameBoard.playerO = true;
-    } else {
-      blank.textContent = "O";
-      gameBoard.playerX = true;
-      gameBoard.playerO = false;
+    displayControl(blank, i);
+    checkWin(gameBoard.playerX);
+    if (turn !== null) {
+      checkWin(gameBoard.playerO);
     }
-    console.log(gameBoard.gameBoard[i].textContent);
   })
 );
+
+restartBtn.addEventListener("click", restart);
